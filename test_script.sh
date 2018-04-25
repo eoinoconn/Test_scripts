d0066b51f6edfe047c3c325c1203cc29eada78ce
13335561 <eoinoconn@gmail.com>
2018-04-25 22:40:04 +0100

    fin

--- test_script.sh
+++ test_script.sh
@@ -2,16 +2,16 @@
 
 echo "started" > status.txt
 
-python full_cifar10_result.py > full_cifar10_result.txt 2>&1 &
+python full_cifar10_result.py > full_cifar10_result.txt 2>&1 &&
 echo "Test 21 complete"
 
-python full_cifar100_result.py > full_cifar100_result.txt 2>&1 &
+python full_cifar100_result.py > full_cifar100_result.txt 2>&1 &&
 echo "Test 22 complete"
 
-python full_fashion_result.py > full_fashion_result.txt 2>&1 &
+python full_fashion_result.py > full_fashion_result.txt 2>&1 &&
 echo "Test 23 complete"
 
-python semi_auto_result.py > semi_auto_result.txt 2>&1 &
+python semi_auto_result.py > semi_auto_result.txt 2>&1 &&
 echo "Test 23 complete"
 
-python random_result.py > random_result.txt 2>&1 &
+python random_result.py > random_result.txt 2>&1 &&
